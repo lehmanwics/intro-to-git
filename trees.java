9a4fafa7707310918f122338ba6a1fa5bd6904f7
lehmanwics <37590177+lehmanwics@users.noreply.github.com>
2018-10-17 12:35:20 -0400

    tree implementation

---
+++ trees.java
@@ -0,0 +1,12 @@
+public class Node {
+  public int value;
+  public Node left;
+  public Node right;
+  
+  public Node(int vavlue, Node left, Node right) {
+  
+  }
+  
+  public Node(int value){
+    this(value, null, null);
+  }
